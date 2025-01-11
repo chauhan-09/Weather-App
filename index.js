c846d694e523db67a2d3fc60115c6edc6f8ad0b2
chauhan-09 <nsc7574@gmail.com>
2025-01-11 22:45:18 +0530

    made changes

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const body = document.querySelector('body');
-const temp = document.querySelector('h1')
+const weather_city = document.querySelector('.city')
 const btn = document.querySelector('.btn');
+const weather = document.querySelector('.weather');
+const apikey = "80300c3c403593e35aec39047d4392ae";
 
-btn.addEventListener('click', () => {
-    temp.innerHTML = "temp is loading...";
+btn.addEventListener('click', () => {  
+    const city = weather_city.value;
+    getweather(city);
 })
+
+
+async function getweather(city)
+{
+    const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+    const response = await fetch(apiurl);
+
+    if(!response.ok) window.alert('Please enter a valid city');
+    else 
+    {
+        const data = await response.json();
+        displayweather(data);
+    }
+}
+
+function displayweather(response)
+{
+    const {name:city,
+        main: {temp,humidity} } = response;
+
+    weather.innerHTML = (temp - 273).toFixed(1) + "°C";
+}
